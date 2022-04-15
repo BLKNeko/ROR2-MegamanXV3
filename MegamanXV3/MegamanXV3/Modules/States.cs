c712bb9ee672e6942a2b6d7fd3965b23857ad3a0
BLKNeko <blackheart2302@gmail.com>
2022-04-14 22:12:38 -0300

    complete posted

--- MegamanXV3/MegamanXV3/Modules/States.cs
+++ MegamanXV3/MegamanXV3/Modules/States.cs
@@ -17,6 +17,24 @@ namespace MegamanXV3.Modules
             Modules.Content.AddEntityState(typeof(Roll));
 
             Modules.Content.AddEntityState(typeof(ThrowBomb));
+
+            Modules.Content.AddEntityState(typeof(shotgunIce));
+            Modules.Content.AddEntityState(typeof(Dash));
+            Modules.Content.AddEntityState(typeof(greenSpinner));
+            Modules.Content.AddEntityState(typeof(meltCreeper));
+            Modules.Content.AddEntityState(typeof(FKBuster));
+            Modules.Content.AddEntityState(typeof(squeezeBomb));
+            Modules.Content.AddEntityState(typeof(FireW));
+            Modules.Content.AddEntityState(typeof(FireW2));
+            Modules.Content.AddEntityState(typeof(FireW3));
+            Modules.Content.AddEntityState(typeof(AcidBurst));
+            Modules.Content.AddEntityState(typeof(AcidBurst2));
+            Modules.Content.AddEntityState(typeof(Unlimited));
+            Modules.Content.AddEntityState(typeof(DeathState));
+            Modules.Content.AddEntityState(typeof(chameleonSting));
+            Modules.Content.AddEntityState(typeof(novaStrike));
+
+
         }
     }
 }
